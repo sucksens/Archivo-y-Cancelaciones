8ca2e5d75e7793893d89bf47ae4681c0b76fe16e
Ernesto Ruiz <jose.ernesto@protonmail.com>
2026-03-27 20:11:11 -0600

    cambios en las vistas
    se metieron cambios en las vistas para arreglar problemas de coors

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -31,8 +31,10 @@
         }
     </script>
     
-    <!-- Heroicons -->
-    <script src="https://unpkg.com/@heroicons/v2.0.18/24/outline/esm/index.js" type="module"></script>
+    <!-- Favicon -->
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📄</text></svg>">
+    
+    <!-- Heroicons (Eliminado script conflictivo de unpkg, se usan SVGs directos) -->
     
     <!-- Estilos personalizados -->
     <style>
