3a1d7451c2a008fd62427dd88a68e1b75c77131f
Ernesto Ruiz <jose.ernesto@protonmail.com>
2026-04-04 21:36:51 -0600

    reformateo de los metodos de facturasbridge

--- app/bbj/facturasBridge.php
+++ app/bbj/facturasBridge.php
@@ -13,52 +13,55 @@ use App\Core\DatabaseBBj;
 
 class FacturasBridge{
 
-    private string $DatabaseName;
-    private DatabaseBBj $db;
+      private string $DatabaseName;
+      private DatabaseBBj $db;
 
-    /**
-     * Constructor de la clase
-     * 
-     * @param string $DatabaseName
-     * se pasa el nombre de la base de datos de bbj en la cual se buscaran operaciones de la factura
-     */
-    public function __construct(string $DatabaseName){
+      /**
+       * Constructor de la clase
+       * 
+       * @param string $DatabaseName
+       * se pasa el nombre de la base de datos de bbj en la cual se buscaran operaciones de la factura
+       */
+      public function __construct(string $DatabaseName){
           $this->DatabaseName = $DatabaseName;
           $this->db = DatabaseBBj::getInstance($this->DatabaseName);
-    }
+      }
 
-    /**
-     * Obtiene la factura de bbj
-     * 
-     * @param string $uuid
-     * @return array
-     */
-     public function getFactura(string $uuid){
-               $sql = "SELECT * FROM FACTURAS WHERE FOLIOFISCAL = ? AND STATUS = '0' ";
-               return $this->db->fetchOne($sql, [$uuid]);
-     }
+      /**
+       * Obtiene la factura de bbj
+       * 
+       * @param string $uuid
+       * @return array
+       */
+      public function getFactura(string $uuid): array
+      {
+            $sql = "SELECT * FROM FACTURAS WHERE FOLIOFISCAL = ? AND STATUS = '0' ";
+            return $this->db->fetchOne($sql, [$uuid]);
+      }
 
-     /**
+      /**
       * Obtiene los recibos de caja de la factura
       * 
       * @param string $vendedor
       * @param string $pedido
       * @return array
       */
-     public function getRecibosCaja(string $vendedor, string $pedido){
-               $sql = "SELECT * FROM PEDCOMPAGOS WHERE ID_VENDEDOR = ? AND ID_PEDIDO = ?";
-               return $this->db->fetchAll($sql, [$vendedor, $pedido]);
-     }
+      public function getRecibosCaja(string $vendedor, string $pedido): array
+      {
+            $sql = "SELECT * FROM PEDCOMPAGOS WHERE ID_VENDEDOR = ? AND ID_PEDIDO = ?";
+            return $this->db->fetchAll($sql, [$vendedor, $pedido]);
+      }
 
-     /**
+      /**
       * Obtiene los documentos relacionados de la factura
       * 
       * @param string $vendedor
       * @param string $pedido
       * @return array
       */
-     public function getDoctosRelacionados(string $vendedor, string $pedido){
-               $sql = "SELECT * FROM NCANTICIPOS WHERE ID_VENDEDOR = ? AND ID_PEDIDO = ? AND STATUS = '0' ";
-               return $this->db->fetchAll($sql, [$vendedor, $pedido]);
-     }
+      public function getDoctosRelacionados(string $vendedor, string $pedido): array
+      {
+            $sql = "SELECT * FROM NCANTICIPOS WHERE ID_VENDEDOR = ? AND ID_PEDIDO = ? AND STATUS = '0' ";
+            return $this->db->fetchAll($sql, [$vendedor, $pedido]);
+      }
 }
